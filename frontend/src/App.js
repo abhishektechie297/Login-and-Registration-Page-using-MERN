5adef509c77ae3035fea4cda8b98a9003283b641
abhishektechie297 <144216574+abhishektechie297@users.noreply.github.com>
2024-11-21 18:32:30 +0530

    adding frontend after few changes

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import RegistrationPage from './RegistrationPage'
+import LoginPage from './LoginPage'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/registration" element={<RegistrationPage/>}/>
+      </Routes>
+      </BrowserRouter>
+      </div>
+  )
+}
+export default App;
